96741f5ba12ae9aa47270c2604787d5a8120daa9
Yop-La <alex.guillemine@gmail.com>
2024-04-04 11:34:01 +0200

    fix to add a env suffix name to lambda fnuction

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "lambda_function1" {
   source = "../../modules/lambda/function1"
-  # Ajoutez ici les variables spécifiques au module, si nécessaire
-  function_name = "my-first-lambda-function-dev"
+  environment   = var.environment
+  function_name = "my-first-lambda-function"
 }
