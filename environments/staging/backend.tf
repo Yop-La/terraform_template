3f73ab8e51c71bfbf5583f19af824d327bd654b2
Yop-La <alex.guillemine@gmail.com>
2024-04-04 10:32:03 +0200

    fix backend tf state

--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "foodles-cloud-functoin-terraform-state"
     key            = "prod/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "nom-de-votre-table-dynamodb"
     encrypt        = true
   }
 }
