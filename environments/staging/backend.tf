e0c7aab8233e97aa6cbcb06d40af8f9addfad8fa
Yop-La <alex.guillemine@gmail.com>
2024-04-04 10:28:40 +0200

    add a remote backend state

---
+++ environments/staging/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "foodles-cloud_functoin-terraform-state"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "nom-de-votre-table-dynamodb"
+    encrypt        = true
+  }
+}
